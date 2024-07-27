336c3635dd03367d7ca75ee9dd1854e0ff9e89cc
Krasnopevtsev Maxim <150928737+Qqruzko@users.noreply.github.com>
2024-07-28 02:22:05 +0300

    Add files via upload

---
+++ lab 15/lab 15/lab 15.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <map>
+int main()
+{
+    std::map<std::string, char> students_grades;
+    // установка значений
+    students_grades["Oleg"] = 'A';
+    students_grades["Anton"] = 'B';
+    students_grades["Kate"] = 'F';
+
+    // получение значений
+    std::cout << "Oleg\t" << students_grades["Oleg"] << std::endl;
+    std::cout << "Anton\t" << students_grades["Anton"] << std::endl;
+    std::cout << "Kate\t" << students_grades["Kate"] << std::endl;
+}
